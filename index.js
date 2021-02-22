1659dcad980f274ca520c7024bd57a33546cd934
“Aaron <github email address>
2021-02-22 12:41:21 -0500

    commit tree 7a31dfadeb2eb0634db4f63c74e48977d165e7f4

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // ***** Question 1 *****
 // *** Uncomment the lines below to test
-// console.log("%cQuestion 1", "color: red") 
+console.log("%cQuestion 1", "color: red") 
 
-// console.log(counter) 
-// // => 2
-// console.log("%c----------", "color: red") 
+console.log(counter) 
+// => 2
+console.log("%c----------", "color: red") 
 
 
 // ***** Question 2 *****
